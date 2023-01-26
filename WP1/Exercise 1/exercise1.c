6bddc1a9639f126ea86fee4635993824a4d7a593
metwally <gusmetwsh@student.gu.se>
2023-01-26 14:08:37 +0100

    comments

--- WP1/Exercise 1/exercise1.c
+++ WP1/Exercise 1/exercise1.c
@@ -5,12 +5,13 @@
 // Exercise 1
 
 int main() {
+    //Initialising variables.
     int point = 1;
     int num;
     while(point==1) {
             printf("Please type a integer from 1 to 5: \n");
-            scanf("%d", &num);
-                    if (num <= 5 || num >0) {
+            scanf("%d", &num); //Reading user input.
+                    if (num <= 5 || num >0) { //Reading user input.
                         if(num == 1){
                             printf("Oh wow you have typed number 1!! \n");}
                         else if (num ==2){
@@ -21,8 +22,8 @@ int main() {
                             printf("You chose option 4.\n");}
                         else if (num ==5){
                             printf("Five is one of my favourite numbers!\n");
-                        // operator doesn't match any case
-                    } else {
+                            
+                    } else { //If the input doesnt match any of the gives options above.
                         printf("You did not give a integer from 1 to 5 as input." );
                         point = 0;
                         }
--- WP1/Exercise 3/exercise3.c
+++ WP1/Exercise 3/exercise3.c
@@ -6,39 +6,41 @@
 #include <time.h>
 
 int main() {
+    //Initialise variables.
     int randomNum;
     int input;
     char opinion;
     int numOfGuess = 0;
+    //Generate a random number with every iteration.
     srand(time(NULL));
     randomNum = rand() % 100;
-    // Generate a random number
+    //Program execution.
     do {
         printf("Would you like to start a round of the game? You have up to 10 tries. Type Y if you want to start a round or F if you want to exit the program: ");
-        scanf("%c", &opinion);
+        scanf("%c", &opinion); //Reading the users input. 
         if (opinion == 'Y') {
             do {
                 printf("Please give a guess between 1 and 100: ");
-                scanf("%d", &input);
-                if (input > randomNum) {
+                scanf("%d", &input); //Reading the users guess.
+                if (input > randomNum) { //If the guess of the user is higher than the correct number.
                     numOfGuess++;
                     printf("Your guess is too high, please give another guess! You have %d more attempts:) \n", 10 - numOfGuess);
-                } else if (randomNum > input) {
+                } else if (randomNum > input) { //If the guess of the user is lower tham the correct number.
                     numOfGuess++;
                     printf("Your guess is too low, please give another guess! You have %d more attempts:) \n ", 10 - numOfGuess);
                 }
-                else {
+                else { //If the guess of the user is correct.
                     numOfGuess++;
                     printf("You have guessed %d times and your guess is correct!", numOfGuess);
                 }
-                if(input > 100 || input < 1){
+                if(input > 100 || input < 1){ //If the input of the user is out of bounds.
                     printf("Out of bounds! Please give a number from 1-100\n");
                 } 
-                if (numOfGuess >= 10){
+                if (numOfGuess >= 10){ //If the user runs out of guesses.
                     printf("Out of guesses, the correct number was %d \n", randomNum);
                 }
             } while (input != randomNum);
-        } else if (opinion == 'F') {
+        } else if (opinion == 'F') { //If the users presses F after the first round or at the very beggining, the program ends.
             return 0;
         }
     }while(1);
