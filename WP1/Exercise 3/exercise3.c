c209966cec7cb461832a935885a843fdf73de590
metwally <gusmetwsh@student.gu.se>
2023-01-26 13:57:37 +0100

    fixes to exercise 3

--- WP1/Exercise 3/exercise3.c
+++ WP1/Exercise 3/exercise3.c
@@ -14,21 +14,28 @@ int main() {
     randomNum = rand() % 100;
     // Generate a random number
     do {
-        printf("Would you like to start a round of the game? Type Y if you want to start a round or F if you want to exit the program: ");
+        printf("Would you like to start a round of the game? You have up to 10 tries. Type Y if you want to start a round or F if you want to exit the program: ");
         scanf("%c", &opinion);
         if (opinion == 'Y') {
             do {
                 printf("Please give a guess between 1 and 100: ");
                 scanf("%d", &input);
                 if (input > randomNum) {
-                    printf("Your guess is too high, please give another guess! \n");
+                    printf("Your guess is too high, please give another guess! You have %d attempts left :) \n", 9 - numOfGuess);
                     numOfGuess++;
                 } else if (randomNum > input) {
-                    printf("Your guess is too low, please give another guess! \n");
+                    printf("Your guess is too low, please give another guess! You have %d attempts left :) \n ", 9 - numOfGuess);
                     numOfGuess++;
-                } else {
+                }
+                else {
                     numOfGuess++;
                     printf("You have guessed %d times and your guess is correct!", numOfGuess);
+                }
+                if(input > 100 || input < 1){
+                    printf("Out of bounds! Please give a number from 1-100\n");
+                } 
+                if (numOfGuess >= 10){
+                    printf("Out of guesses, the correct number was %d \n", randomNum);
                 }
             } while (input != randomNum);
         } else if (opinion == 'F') {
