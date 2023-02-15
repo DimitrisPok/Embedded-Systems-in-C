28983f3f76326911ade00001eba0b720e835f2dd
metwally <gusmetwsh@student.gu.se>
2023-02-15 16:28:14 +0100

    WP4 exercise 3

---
+++ WP4/Exercise 3/exercise3.c
@@ -0,0 +1,69 @@
+//neopixel library import
+#include <Adafruit_NeoPixel.h> 
+
+//variable declaration
+int sensorAnalog = 0; //for analog reading
+int neoPin = 2; //neopixel digital pin
+int pixelNum = 24; //number of pixels
+int maxled; //maxled variable initialization
+
+//neopixel object declaration
+Adafruit_NeoPixel neopixel = Adafruit_NeoPixel(pixelNum, neoPin, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+  Serial.begin(9600); //serial begin
+  neopixel.begin(); //neopixel start
+  neopixel.setBrightness(255); //set neopixel to max brightness
+  neopixel.show(); //neopixel show
+}
+
+void loop()
+{
+ 
+ //save analog reading from the temperature sensor to a variable
+ int read = analogRead(sensorAnalog);
+ //convert read to celsius
+ int temperatureC = map(((read - 20) * 3.04), 0, 1023, -40, 125);
+ 
+ //serial print temperature
+ Serial.print(temperatureC); Serial.println(" degrees C");
+ 
+ //allocating the temperature to the led (max pixel number/ max temperature)
+ maxled= read*pixelNum/358;
+ 
+ //color variables
+ uint32_t red = neopixel.Color(255, 0, 0); //red
+ uint32_t green = neopixel.Color(0, 255, 0);//green
+ uint32_t blue = neopixel.Color(0, 0, 255); //blue
+ uint32_t orange = neopixel.Color(255, 100, 10); //orange
+ uint32_t magenta = neopixel.Color(255, 0, 255); //magenta
+  
+  //for each pixel in the neopixel 
+  for (int i=0; i<pixelNum; i++){
+    //if i (the pixel we are currently on) is less than maxled..
+    if(i<=maxled){
+      //..set the pixel color
+      //allocate the color to the temperature intervals
+      if (temperatureC<=-25){ 
+         neopixel.setPixelColor(i, blue);
+      }
+      else if (temperatureC<=0){
+        neopixel.setPixelColor(i, magenta);
+      }
+      else if (temperatureC<=25) {
+        neopixel.setPixelColor(i, green);
+      }
+      else if (temperatureC<=50){
+        neopixel.setPixelColor(i, orange);
+      }
+      else{
+        neopixel.setPixelColor(i, red);
+      }
+    } else { //else close the pixel
+      neopixel.setPixelColor(i,0,0,0);
+    }
+    }
+  	delay(100); //delay by 100 ms
+    neopixel.show(); //neopixel show
+}
