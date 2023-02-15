09e3f1e3c25e8d29bf4bbcb734dd547fdf9c7208
metwally <gusmetwsh@student.gu.se>
2023-02-15 19:08:32 +0100

    exercise 4 search

---
+++ WP4/Exercise 4/exercise4.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int search_number(int number, int tab[], int size){
+    int found;
+    int i=0;
+    for(i=0; i<size; i++){
+        if(tab[i] == number){
+            found = 1;
+            break;
+        }
+    }
+    if(found == 1){
+        printf("%d is found at position %d", number, i+1);
+        return 0;
+    }else{
+        printf("%d was not found in the array", number);
+        return -1;
+    }
+}
+
+int main(int argc, char *argv[]){
+    int toSearch;
+    int test[] = {1,2,34,5,67,3,23,12,13,10}; 
+    printf("Enter an element to search: ");
+    scanf("%d", &toSearch);
+    search_number(toSearch, test, sizeof(test));
+    return 0;
+}
