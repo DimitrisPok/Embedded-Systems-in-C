30631585b01ab7da218f147db3226a465e27424f
Sepehr <sepehrmo@student.chalmers.se>
2023-02-15 19:29:41 +0100

    sort exercise 4

--- WP4/Exercise 4/exercise4.c
+++ WP4/Exercise 4/exercise4.c
@@ -20,11 +20,21 @@ int search_number(int number, int tab[], int size){
     }
 }
 
+void sort (int number, int tab []) {
+    int i, j;
+    for (i = 0; i < number - 1; i++)
+        for (j = 0; j < number - i - 1; j++)
+            if (tab[j] > tab[j + 1])
+                swap(&tab[j], &tab[j + 1]);
+}
+
 int main(int argc, char *argv[]){
     int toSearch;
     int test[] = {1,2,34,5,67,3,23,12,13,10}; 
+    int number = sizeof(test) / sizeof(test[0]);
     printf("Enter an element to search: ");
     scanf("%d", &toSearch);
     search_number(toSearch, test, sizeof(test));
+    sort(number, test);
     return 0;
 }
